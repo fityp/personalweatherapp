f2fd255ecb5a9d2c9ba0db23f7f12845ae9c8f8d
Dustin Fitzpatrick <dustin.fitzpatrick@disney.com>
2020-08-28 07:41:30 -0400

    Initial Commit

---
+++ src/main/java/org/fsu/cs/capstone/personalweatherapplication/PwaPersonalWeatherApplication.java
@@ -0,0 +1,13 @@
+package org.fsu.cs.capstone.personalweatherapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwaPersonalWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwaPersonalWeatherApplication.class, args);
+	}
+
+}
