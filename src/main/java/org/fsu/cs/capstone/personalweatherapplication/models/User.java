3829a877713f34c29d8180493bce84d86ac2b81d
Devin Rucks <blindnives@gmail.com>
2020-09-21 17:44:57 -0400

    Created post mapping to recieve username and password from client, also saves values to db without authorization or encryption for testing purposes

--- src/main/java/org/fsu/cs/capstone/personalweatherapplication/models/User.java
+++ src/main/java/org/fsu/cs/capstone/personalweatherapplication/models/User.java
@@ -15,6 +15,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    private String password;
     private String email;
 
 }
