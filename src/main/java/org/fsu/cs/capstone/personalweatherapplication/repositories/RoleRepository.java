98475d6edb5d125d60dec55e4dd5c4d31fde6a54
Devin Rucks <blindnives@gmail.com>
2020-09-29 15:45:55 -0400

    Modified RoleRepository

--- src/main/java/org/fsu/cs/capstone/personalweatherapplication/repositories/RoleRepository.java
+++ src/main/java/org/fsu/cs/capstone/personalweatherapplication/repositories/RoleRepository.java
@@ -1,2 +1,7 @@
-package org.fsu.cs.capstone.personalweatherapplication.repositories;public interface RoleRepository {
+package org.fsu.cs.capstone.personalweatherapplication.repositories;
+
+import org.fsu.cs.capstone.personalweatherapplication.models.UserRole;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<UserRole, Long> {
 }
