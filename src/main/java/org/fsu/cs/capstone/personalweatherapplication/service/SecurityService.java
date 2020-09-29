ebef0bd492ccfeec84948b0e64c9ba0ff7f33adc
Devin Rucks <blindnives@gmail.com>
2020-09-29 15:47:15 -0400

    Modified SecurityService

--- src/main/java/org/fsu/cs/capstone/personalweatherapplication/service/SecurityService.java
+++ src/main/java/org/fsu/cs/capstone/personalweatherapplication/service/SecurityService.java
@@ -1,2 +1,9 @@
-package org.fsu.cs.capstone.personalweatherapplication.service;public class SecurityService {
+package org.fsu.cs.capstone.personalweatherapplication.service;
+
+
+// SecurityService provides current logged-in user and auto login after user registration.
+public interface SecurityService {
+    String findLoggedInUsername();
+
+    void autoLogin(String username, String password);
 }
