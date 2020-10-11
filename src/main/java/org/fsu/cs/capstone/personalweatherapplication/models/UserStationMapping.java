8ecf713e4eac4eaf92e03e95ecc23b2ad7b815dd
Brandon <brandman546@gmail.com>
2020-10-11 17:02:38 -0500

    Added Models for stations and user station mapping table
    Will need to discuss if these will be JPA managed objects.

---
+++ src/main/java/org/fsu/cs/capstone/personalweatherapplication/models/UserStationMapping.java
@@ -0,0 +1,9 @@
+package org.fsu.cs.capstone.personalweatherapplication.models;
+
+import lombok.Data;
+
+@Data
+public class UserStationMapping {
+    private int station_id;
+    private int user_id;
+}
